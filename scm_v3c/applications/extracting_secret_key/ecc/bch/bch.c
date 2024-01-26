723b093a751a9ec414100bde5d2dac0163b0deab
bkorecic <blaz.korecic@ug.uchile.cl>
2024-01-26 12:00:52 +0100

    Add linux bch.c/bch.h implementations

--- scm_v3c/applications/extracting_secret_key/ecc/bch/bch.c
+++ scm_v3c/applications/extracting_secret_key/ecc/bch/bch.c
@@ -65,59 +65,27 @@
  * finite fields GF(2^q). In Rapport de recherche INRIA no 2829, 1996.
  */
 
-#if defined(__KERNEL__)
-
-# include <linux/kernel.h>
-# include <linux/errno.h>
-# include <linux/init.h>
-# include <linux/module.h>
-# include <linux/slab.h>
-# include <linux/bitops.h>
-# include <asm/byteorder.h>
-# include <linux/bch.h>
-
-#else
-
-# define _BSD_SOURCE
-# define _DEFAULT_SOURCE
-# if !defined(htobe32)
-#  if BYTE_ORDER == LITTLE_ENDIAN
-#   define htobe32(x) htonl(x)
-#  elif BYTE_ORDER == BIG_ENDIAN
-#   define htobe32(x) (x)
-#  endif
-# endif
-# include <errno.h>
-# include <stdint.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
-# include "bch.h"
-# define MODULE_LICENSE(s)
-# define MODULE_AUTHOR(s)
-# define MODULE_DESCRIPTION(s)
-# define EXPORT_SYMBOL_GPL(x)
-# define GFP_KERNEL
-# define kmalloc(size, flags) malloc(size)
-# define kzalloc(size, flags) memset(malloc(size), 0, size)
-# define kfree(ptr) free(ptr)
-# define DIV_ROUND_UP(a, b) ((a + b - 1) / b)
-# define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(*(arr)))
-# define cpu_to_be32(x) htobe32(x)
-# define WARN_ON(x) (x)
-#endif
+#include <linux/kernel.h>
+#include <linux/errno.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/slab.h>
+#include <linux/bitops.h>
+#include <linux/bitrev.h>
+#include <asm/byteorder.h>
+#include <linux/bch.h>
 
 #if defined(CONFIG_BCH_CONST_PARAMS)
 #define GF_M(_p)               (CONFIG_BCH_CONST_M)
 #define GF_T(_p)               (CONFIG_BCH_CONST_T)
 #define GF_N(_p)               ((1 << (CONFIG_BCH_CONST_M))-1)
 #define BCH_MAX_M              (CONFIG_BCH_CONST_M)
-#define BCH_MAX_T              (CONFIG_BCH_CONST_T)
+#define BCH_MAX_T	       (CONFIG_BCH_CONST_T)
 #else
 #define GF_M(_p)               ((_p)->m)
 #define GF_T(_p)               ((_p)->t)
 #define GF_N(_p)               ((_p)->n)
-#define BCH_MAX_M              31 /* 2KB */
+#define BCH_MAX_M              15 /* 2KB */
 #define BCH_MAX_T              64 /* 64 bit correction */
 #endif
 
@@ -127,7 +95,7 @@
 #define BCH_ECC_MAX_WORDS      DIV_ROUND_UP(BCH_MAX_M * BCH_MAX_T, 32)
 
 #ifndef dbg
-#define dbg(_fmt, ...)     do {} while (0)
+#define dbg(_fmt, args...)     do {} while (0)
 #endif
 
 /*
@@ -143,65 +111,16 @@ struct gf_poly {
 
 /* polynomial of degree 1 */
 struct gf_poly_deg1 {
-    struct {
-        unsigned int deg;
-    } poly;
+	struct gf_poly poly;
 	unsigned int   c[2];
 };
 
-static uint8_t swap_bits_table[] = {
-	0x00, 0x80, 0x40, 0xc0, 0x20, 0xa0, 0x60, 0xe0,
-	0x10, 0x90, 0x50, 0xd0, 0x30, 0xb0, 0x70, 0xf0,
-	0x08, 0x88, 0x48, 0xc8, 0x28, 0xa8, 0x68, 0xe8,
-	0x18, 0x98, 0x58, 0xd8, 0x38, 0xb8, 0x78, 0xf8,
-	0x04, 0x84, 0x44, 0xc4, 0x24, 0xa4, 0x64, 0xe4,
-	0x14, 0x94, 0x54, 0xd4, 0x34, 0xb4, 0x74, 0xf4,
-	0x0c, 0x8c, 0x4c, 0xcc, 0x2c, 0xac, 0x6c, 0xec,
-	0x1c, 0x9c, 0x5c, 0xdc, 0x3c, 0xbc, 0x7c, 0xfc,
-	0x02, 0x82, 0x42, 0xc2, 0x22, 0xa2, 0x62, 0xe2,
-	0x12, 0x92, 0x52, 0xd2, 0x32, 0xb2, 0x72, 0xf2,
-	0x0a, 0x8a, 0x4a, 0xca, 0x2a, 0xaa, 0x6a, 0xea,
-	0x1a, 0x9a, 0x5a, 0xda, 0x3a, 0xba, 0x7a, 0xfa,
-	0x06, 0x86, 0x46, 0xc6, 0x26, 0xa6, 0x66, 0xe6,
-	0x16, 0x96, 0x56, 0xd6, 0x36, 0xb6, 0x76, 0xf6,
-	0x0e, 0x8e, 0x4e, 0xce, 0x2e, 0xae, 0x6e, 0xee,
-	0x1e, 0x9e, 0x5e, 0xde, 0x3e, 0xbe, 0x7e, 0xfe,
-	0x01, 0x81, 0x41, 0xc1, 0x21, 0xa1, 0x61, 0xe1,
-	0x11, 0x91, 0x51, 0xd1, 0x31, 0xb1, 0x71, 0xf1,
-	0x09, 0x89, 0x49, 0xc9, 0x29, 0xa9, 0x69, 0xe9,
-	0x19, 0x99, 0x59, 0xd9, 0x39, 0xb9, 0x79, 0xf9,
-	0x05, 0x85, 0x45, 0xc5, 0x25, 0xa5, 0x65, 0xe5,
-	0x15, 0x95, 0x55, 0xd5, 0x35, 0xb5, 0x75, 0xf5,
-	0x0d, 0x8d, 0x4d, 0xcd, 0x2d, 0xad, 0x6d, 0xed,
-	0x1d, 0x9d, 0x5d, 0xdd, 0x3d, 0xbd, 0x7d, 0xfd,
-	0x03, 0x83, 0x43, 0xc3, 0x23, 0xa3, 0x63, 0xe3,
-	0x13, 0x93, 0x53, 0xd3, 0x33, 0xb3, 0x73, 0xf3,
-	0x0b, 0x8b, 0x4b, 0xcb, 0x2b, 0xab, 0x6b, 0xeb,
-	0x1b, 0x9b, 0x5b, 0xdb, 0x3b, 0xbb, 0x7b, 0xfb,
-	0x07, 0x87, 0x47, 0xc7, 0x27, 0xa7, 0x67, 0xe7,
-	0x17, 0x97, 0x57, 0xd7, 0x37, 0xb7, 0x77, 0xf7,
-	0x0f, 0x8f, 0x4f, 0xcf, 0x2f, 0xaf, 0x6f, 0xef,
-	0x1f, 0x9f, 0x5f, 0xdf, 0x3f, 0xbf, 0x7f, 0xff,
-};
-
-#if !defined(__APPLE__)
-static inline unsigned int fls(unsigned int x)
-{
-	unsigned int count = 0;
-
-	while (x >>= 1) {
-		count++;
-	}
-	return count + 1;
-}
-#endif
-
-static uint8_t swap_bits(struct bch_control *bch, uint8_t in)
+static u8 swap_bits(struct bch_control *bch, u8 in)
 {
 	if (!bch->swap_bits)
 		return in;
 
-	return swap_bits_table[in];
+	return bitrev8(in);
 }
 
 /*
@@ -216,7 +135,7 @@ static void bch_encode_unaligned(struct bch_control *bch,
 	const int l = BCH_ECC_WORDS(bch)-1;
 
 	while (len--) {
-		uint8_t tmp = swap_bits(bch, *data++);
+		u8 tmp = swap_bits(bch, *data++);
 
 		p = bch->mod8_tab + (l+1)*(((ecc[0] >> 24)^(tmp)) & 0xff);
 
@@ -237,15 +156,15 @@ static void load_ecc8(struct bch_control *bch, uint32_t *dst,
 	unsigned int i, nwords = BCH_ECC_WORDS(bch)-1;
 
 	for (i = 0; i < nwords; i++, src += 4)
-		dst[i] = ((uint32_t)swap_bits(bch, src[0]) << 24) |
-			((uint32_t)swap_bits(bch, src[1]) << 16) |
-			((uint32_t)swap_bits(bch, src[2]) << 8) |
+		dst[i] = ((u32)swap_bits(bch, src[0]) << 24) |
+			((u32)swap_bits(bch, src[1]) << 16) |
+			((u32)swap_bits(bch, src[2]) << 8) |
 			swap_bits(bch, src[3]);
 
 	memcpy(pad, src, BCH_ECC_BYTES(bch)-4*nwords);
-	dst[nwords] = ((uint32_t)swap_bits(bch, pad[0]) << 24) |
-		((uint32_t)swap_bits(bch, pad[1]) << 16) |
-		((uint32_t)swap_bits(bch, pad[2]) << 8) |
+	dst[nwords] = ((u32)swap_bits(bch, pad[0]) << 24) |
+		((u32)swap_bits(bch, pad[1]) << 16) |
+		((u32)swap_bits(bch, pad[2]) << 8) |
 		swap_bits(bch, pad[3]);
 }
 
@@ -340,10 +259,10 @@ void bch_encode(struct bch_control *bch, const uint8_t *data,
 		/* input data is read in big-endian format */
 		w = cpu_to_be32(*pdata++);
 		if (bch->swap_bits)
-			w = (uint32_t)swap_bits(bch, w) |
-			    ((uint32_t)swap_bits(bch, w >> 8) << 8) |
-			    ((uint32_t)swap_bits(bch, w >> 16) << 16) |
-			    ((uint32_t)swap_bits(bch, w >> 24) << 24);
+			w = (u32)swap_bits(bch, w) |
+			    ((u32)swap_bits(bch, w >> 8) << 8) |
+			    ((u32)swap_bits(bch, w >> 16) << 16) |
+			    ((u32)swap_bits(bch, w >> 24) << 24);
 		w ^= r[0];
 		p0 = tab0 + (l+1)*((w >>  0) & 0xff);
 		p1 = tab1 + (l+1)*((w >>  8) & 0xff);
@@ -445,17 +364,6 @@ static inline int a_ilog(struct bch_control *bch, unsigned int x)
 	return mod_s(bch, GF_N(bch)-bch->a_log_tab[x]);
 }
 
-void bch_compute_even_syndromes(struct bch_control *bch, unsigned int *syn)
-{
-	int i;
-	const int t = GF_T(bch);
-
-	/* v(a^(2j)) = v(a^j)^2 */
-	for (i = 0; i < t; i++)
-		syn[2*i+1] = gf_sqr(bch, syn[i]);
-}
-EXPORT_SYMBOL_GPL(bch_compute_even_syndromes);
-
 /*
  * compute 2t syndromes of ecc polynomial, i.e. ecc(a^j) for j=1..2t
  */
@@ -488,7 +396,9 @@ static void compute_syndromes(struct bch_control *bch, uint32_t *ecc,
 		}
 	} while (s > 0);
 
-    bch_compute_even_syndromes(bch, syn);
+	/* v(a^(2j)) = v(a^j)^2 */
+	for (j = 0; j < t; j++)
+		syn[2*j+1] = gf_sqr(bch, syn[j]);
 }
 
 static void gf_poly_copy(struct gf_poly *dst, struct gf_poly *src)
@@ -634,7 +544,7 @@ static int find_affine4_roots(struct bch_control *bch, unsigned int a,
 {
 	int i, j, k;
 	const int m = GF_M(bch);
-	unsigned int mask = 0xffff, t, rows[32] = {0,};
+	unsigned int mask = 0xff, t, rows[16] = {0,};
 
 	j = a_log(bch, b);
 	k = a_log(bch, a);
@@ -649,10 +559,10 @@ static int find_affine4_roots(struct bch_control *bch, unsigned int a,
 		k += 2;
 	}
 	/*
-	 * transpose 32x32 matrix before passing it to linear solver
-	 * warning: this code assumes m < 32
+	 * transpose 16x16 matrix before passing it to linear solver
+	 * warning: this code assumes m < 16
 	 */
-	for (j = 16; j != 0; j >>= 1, mask ^= (mask << j)) {
+	for (j = 8; j != 0; j >>= 1, mask ^= (mask << j)) {
 		for (k = 0; k < 16; k = (k+j+1) & ~j) {
 			t = ((rows[k] >> j)^rows[k+j]) & mask;
 			rows[k] ^= (t << j);
@@ -983,7 +893,7 @@ static void factor_polynomial(struct bch_control *bch, int k, struct gf_poly *f,
 			/* compute h=f/gcd(f,tk); this will modify f and q */
 			gf_poly_div(bch, f, gcd, q);
 			/* store g and h in-place (clobbering f) */
-			*h = (struct gf_poly *) &((struct gf_poly_deg1 *)f)[gcd->deg].poly;
+			*h = &((struct gf_poly_deg1 *)f)[gcd->deg].poly;
 			gf_poly_copy(*g, gcd);
 			gf_poly_copy(*h, q);
 		}
@@ -1352,7 +1262,7 @@ finish:
 
 /**
  * bch_init - initialize a BCH encoder/decoder
- * @m:          Galois field order, should be in the range 5-31
+ * @m:          Galois field order, should be in the range 5-15
  * @t:          maximum error correction capability, in bits
  * @prim_poly:  user-provided primitive polynomial (or 0 to use default)
  * @swap_bits:  swap bits within data and syndrome bytes
@@ -1398,9 +1308,9 @@ struct bch_control *bch_init(int m, int t, unsigned int prim_poly,
 #endif
 	if ((m < min_m) || (m > BCH_MAX_M))
 		/*
-		 * values of m greater than 31 are not currently supported;
-		 * supporting m > 31 would require changing table base type
-		 * (uint32_t) and a small patch in matrix transposition
+		 * values of m greater than 15 are not currently supported;
+		 * supporting m > 15 would require changing table base type
+		 * (uint16_t) and a small patch in matrix transposition
 		 */
 		goto fail;
 
@@ -1439,7 +1349,6 @@ struct bch_control *bch_init(int m, int t, unsigned int prim_poly,
 	bch->cache     = bch_alloc(2*t*sizeof(*bch->cache), &err);
 	bch->elp       = bch_alloc((t+1)*sizeof(struct gf_poly_deg1), &err);
 	bch->swap_bits = swap_bits;
-	bch->prim_poly = prim_poly;
 
 	for (i = 0; i < ARRAY_SIZE(bch->poly_2t); i++)
 		bch->poly_2t[i] = bch_alloc(GF_POLY_SZ(2*t), &err);
--- scm_v3c/applications/extracting_secret_key/ecc/bch/bch.h
+++ scm_v3c/applications/extracting_secret_key/ecc/bch/bch.h
@@ -14,12 +14,7 @@
 #ifndef _BCH_H
 #define _BCH_H
 
-#if defined(__KERNEL__)
 #include <linux/types.h>
-#else
-#include <stdbool.h>
-#include <stdint.h>
-#endif
 
 /**
  * struct bch_control - BCH control structure
@@ -46,10 +41,9 @@ struct bch_control {
 	unsigned int    t;
 	unsigned int    ecc_bits;
 	unsigned int    ecc_bytes;
-	unsigned int    prim_poly;
 /* private: */
-	uint32_t       *a_pow_tab;
-	uint32_t       *a_log_tab;
+	uint16_t       *a_pow_tab;
+	uint16_t       *a_log_tab;
 	uint32_t       *mod8_tab;
 	uint32_t       *ecc_buf;
 	uint32_t       *ecc_buf2;
@@ -58,7 +52,7 @@ struct bch_control {
 	int            *cache;
 	struct gf_poly *elp;
 	struct gf_poly *poly_2t[4];
-	bool           swap_bits;
+	bool		swap_bits;
 };
 
 struct bch_control *bch_init(int m, int t, unsigned int prim_poly,
@@ -68,8 +62,6 @@ void bch_free(struct bch_control *bch);
 
 void bch_encode(struct bch_control *bch, const uint8_t *data,
 		unsigned int len, uint8_t *ecc);
-
-void bch_compute_even_syndromes(struct bch_control *bch, unsigned int *syn);
 
 int bch_decode(struct bch_control *bch, const uint8_t *data, unsigned int len,
 	       const uint8_t *recv_ecc, const uint8_t *calc_ecc,
