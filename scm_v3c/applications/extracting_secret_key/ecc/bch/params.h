aadd5229105fd4a13a8e1c92fa492f15a172c6ca
bkorecic <blaz.korecic@ug.uchile.cl>
2024-01-26 12:29:01 +0100

    Params configuration

--- scm_v3c/applications/extracting_secret_key/ecc/bch/params.h
+++ scm_v3c/applications/extracting_secret_key/ecc/bch/params.h
@@ -1,8 +1,9 @@
 #ifndef __PARAMS_H
 #define __PARAMS_H
 
-#include <stdio.h>
-
+#define CONFIG_BCH_CONST_PARAMS
+#define CONFIG_BCH_CONST_M 7 // Galois field order
+#define CONFIG_BCH_CONST_T 1 // Error correction capability in bits
 #define PUF_ADDR ((uint8_t*)0x20005000) // adequate SCuM SRAM address for PUF
 #define KEY_LEN 64
 #define BYTE_SIZE 8
